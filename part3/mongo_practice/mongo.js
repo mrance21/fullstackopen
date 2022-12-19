3bccb1c021de07a3183ea340c2f2481a5623662c
mrance21 <mrance21@gmail.com>
2022-12-18 20:05:45 -0500

    practice database with mongo

---
+++ part3/mongo_practice/mongo.js
@@ -0,0 +1,47 @@
+// const express = require('express')
+// const app = express()
+const mongoose = require('mongoose');
+
+// mongoose.set('strictQuery', true);
+
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+  }
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://mrance21:Giovanni21@cluster0.ndtepta.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected')
+
+    // const note = new Note({
+    //   content: 'print this note to the console',
+    //   date: new Date(),
+    //   important: true,
+    // })
+    // return note.save()
+  })
+//   .then(() => {
+//     console.log('note saved!')
+//     return mongoose.connection.close()
+//   })
+Note.find({}).then(result => {
+    result.forEach(note => {
+    console.log(note)
+    })
+    mongoose.connection.close()
+})
+  .catch((err) => console.log(err))

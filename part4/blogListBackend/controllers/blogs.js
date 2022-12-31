7b03392b8f1c8f0bbc591d6b94b509c070f5ce80
mrance21 <mrance21@gmail.com>
2022-12-31 02:27:01 -0500

    added token authentication to post request

--- part4/blogListBackend/controllers/blogs.js
+++ part4/blogListBackend/controllers/blogs.js
@@ -1,17 +1,46 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate({ username: 1, name: 1 })
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogRouter.post('/', async (request, response) => {
-  const newBlog = new Blog(request.body)
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
-  const savedBlog = await newBlog.save()
-  response.status(201).json(savedBlog)
+blogRouter.post('/', async (request, response) => {
+  const body =(request.body)
+
+  const token = getTokenFrom(request)
+  /* Verifying the token. */
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blog = user.blog.concat(savedBlog._id)
+  await user.save()
+
+  response.json(savedBlog)
 })
 
 blogRouter.delete('/:id', async (request, response) => {

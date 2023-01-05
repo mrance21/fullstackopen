c9905c14bf4834d5809e79dc1b32e47e8c126b0a
mrance21 <mrance21@gmail.com>
2023-01-04 20:28:00 -0500

    added Togglable.displayName

--- part2/notes/src/components/Togglable.js
+++ part2/notes/src/components/Togglable.js
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types'
-import { useState, forwardRef, useImperativeHandle } from "react";
+import { useState, forwardRef, useImperativeHandle } from 'react'
 
 /* `forwardRef` is a function that is used to pass a ref to a component. */
 const Togglable = forwardRef((props, refs) => {
 /* A hook that is used to set the state of the component. */
   const [visible, setVisible] = useState(false)
-/* Setting the display property of the div to none if the value of visible is false. */
-  const hideWhenVisible = {display: visible ? 'none' : ''}
-/* Setting the display property of the div to none if the value of visible is false. */
-  const showWhenVisible = {display: visible ? '' : 'none'}
+
+  /* Setting the display property of the div to none if the value of visible is false. */
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  /* Setting the display property of the div to none if the value of visible is false. */
+  const showWhenVisible = { display: visible ? '' : 'none' }
 
 
- /**
+  /**
   * If the current value of visible is true, set it to false. If the current value of visible is false,
   * set it to true
   */
@@ -45,4 +46,5 @@ Togglable.propTypes = {
   buttonLabel: PropTypes.string.isRequired
 }
 
+Togglable.displayName = 'Togglable'
 export default Togglable
--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -3,33 +3,33 @@ const baseUrl = '/api/notes'
 
 let token = null
 const setToken = newToken => {
-    token = `bearer ${newToken}`
+  token = `bearer ${newToken}`
 }
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    const nonExisting = {
-        id: 10000,
-        content: 'This note is not saved to server',
-        date: '2019-05-30T17:30:31.098Z',
-        important: true,
-    }
-    return request.then(response => response.data.concat(nonExisting))
-    // const request = axios.get(baseUrl)
-    // return request.then(response => response.data)
+  const request = axios.get(baseUrl)
+  const nonExisting = {
+    id: 10000,
+    content: 'This note is not saved to server',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true,
+  }
+  return request.then(response => response.data.concat(nonExisting))
+  // const request = axios.get(baseUrl)
+  // return request.then(response => response.data)
 }
 
 const create = async newObject => {
-    const config = {
-        headers : { Authorization: token }
-    }
-    const response = await axios.post(baseUrl, newObject, config)
-    return response.data
+  const config = {
+    headers : { Authorization: token }
+  }
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
 }
 
 const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => response.data)
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
 }
 
 const noteService =  { getAll, create, update, setToken }

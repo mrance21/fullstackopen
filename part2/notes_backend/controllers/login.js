73ed2a2bf575cce2d0ea08418af5dd93ae8890e4
mrance21 <mrance21@gmail.com>
2023-01-05 00:26:45 -0500

    added script

--- part2/notes_backend/controllers/login.js
+++ part2/notes_backend/controllers/login.js
@@ -5,13 +5,14 @@ const User = require('../models/user')
 
 loginRouter.post('/', async (request, response) => {
   const { username, password } = request.body
+  console.log(username)
 
   /* Finding the user with the username that was given in the request body. */
   const user = await User.findOne({ username })
+  console.log(user)
   const passwordCorrect = user === null
     ? false
     : await bcrypt.compare(password, user.passwordHash)
-
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
       error: 'invalid username or password'

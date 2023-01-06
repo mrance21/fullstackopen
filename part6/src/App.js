b5b7de3898c3585b654d92658fb5bb8cc7b0bd34
mrance21 <mrance21@gmail.com>
2023-01-05 21:58:01 -0500

    fixed parameter of configureStore

--- part6/src/App.js
+++ part6/src/App.js
@@ -1,29 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { configureStore } from '@reduxjs/toolkit'
-
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
-  }
-}
-
-const store = configureStore(counterReducer)
-console.log(store.getState())
-store.dispatch({ type: 'INCREMENT' })
-store.dispatch({ type: 'INCREMENT' })
-store.dispatch({ type: 'INCREMENT' })
-console.log(store.getState())
-store.dispatch({ type: 'ZERO' })
-store.dispatch({ type: 'DECREMENT' })
-console.log(store.getState())
 
 function App() {
   return (

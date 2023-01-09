0863ea090f91963b2cb26e94eb259a8f89d83003
mrance21 <mrance21@gmail.com>
2023-01-08 19:49:28 -0500

    changed index module

--- part6/simple_note/src/index.js
+++ part6/simple_note/src/index.js
@@ -26,6 +26,24 @@ store.dispatch({
   }
 })
 
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    data: { id }
+  }
+}
+
 const generateId = () =>
   Number((Math.random() * 1000000).toFixed(0))
 
@@ -34,21 +52,11 @@ const App = () => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    store.dispatch({
-      type: 'NEW_NOTE',
-      data: {
-        content,
-        important: false,
-        id: generateId()
-      }
-    })
+    store.dispatch(createNote(content))
   }
 
   const toggleImportance = (id) => {
-    store.dispatch({
-      type: 'TOGGLE_IMPORTANCE',
-      data: { id }
-    })
+    store.dispatch(toggleImportanceOf(id))
   }
 
   return (
